2a1e2f9498b33065253da4c7367fff4f4e9dc02c
noteandcode <piroska.rakoczi@gmail.com>
2025-09-05 13:55:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 import io
+from selenium import webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(options=options)
 
 app = Flask(__name__)
 app.secret_key = "supersecret"  # szükséges a session-höz
